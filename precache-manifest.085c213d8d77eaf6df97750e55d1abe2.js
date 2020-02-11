7470f2f24f04613942680ca29e856cdd6a53b514
kumarvaibhav45 <wave.vaibhav@gmail.com>
2020-02-11 15:46:57 +0530

    Updates

---
+++ precache-manifest.085c213d8d77eaf6df97750e55d1abe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9267a3da870a371fbd263c64a8cfa7",
+    "url": "/binary-to-decimal-converter/index.html"
+  },
+  {
+    "revision": "313fef856c20931475a8",
+    "url": "/binary-to-decimal-converter/static/css/main.0c7e7b7a.chunk.css"
+  },
+  {
+    "revision": "7899daeaff978d2c5e40",
+    "url": "/binary-to-decimal-converter/static/js/2.82156f04.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/binary-to-decimal-converter/static/js/2.82156f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "313fef856c20931475a8",
+    "url": "/binary-to-decimal-converter/static/js/main.aebddfab.chunk.js"
+  },
+  {
+    "revision": "44db85775c6f530dab6b",
+    "url": "/binary-to-decimal-converter/static/js/runtime-main.746a46fb.js"
+  },
+  {
+    "revision": "84b582545ee98c6b9bd991ad5bca66bc",
+    "url": "/binary-to-decimal-converter/static/media/cog.84b58254.svg"
+  }
+]);
